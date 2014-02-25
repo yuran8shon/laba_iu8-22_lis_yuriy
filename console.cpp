fb43201808d1cf28834dfcb75dee20522d6c90f3
yuriy <lis-yuran@rambler.ru>
2014-02-25 16:46:48 +0400

    laba_iu8-22_7_cu

---
+++ console.cpp
@@ -0,0 +1,13 @@
+// console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hi");
+	// int lol, comment;
+	return 0;
+}
+
